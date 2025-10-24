25f19e9c03d7248aba4e8f5001be2c0e4279abbd
tabortoa <815141681@qq.com>
2025-10-24 17:32:48 +0800

    fix(frontend): 更新金币图标样式并移除编辑心愿页面的图标
    修改Font Awesome金币图标为4.7.0版本可用的图标
    移除编辑心愿页面中金币输入框左侧的图标

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -3225,10 +3225,8 @@ function showEditWishModal(wish) {
                 <div class="mb-4">
                     <label class="block text-sm font-medium text-gray-700 mb-1">所需金币</label>
                     <div class="relative">
-                        <span class="absolute left-4 top-1/2 -translate-y-1/2 text-yellow-500">
-                            <i class="fa fa-coins"></i>
-                        </span>
-                        <input type="number" id="edit-wish-cost" min="1" class="w-full pl-10 pr-4 py-2 border border-gray-300 rounded-xl focus:ring-2 focus:ring-yellow-400 focus:border-transparent outline-none">
+                        <!-- 移除金币图标 -->
+                        <input type="number" id="edit-wish-cost" min="1" class="w-full px-4 py-2 border border-gray-300 rounded-xl focus:ring-2 focus:ring-yellow-400 focus:border-transparent outline-none">
                     </div>
                 </div>
                 
